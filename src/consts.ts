4092d880f92adca10401f28f68b458ef2e17fb1c
Alwyn-Tan <2660412180@qq.com>
2026-05-15 15:58:16 +0800

    change Home and About

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Alwyn\'s Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Alwyn Tan\'s Space';
+export const SITE_DESCRIPTION = 'Notes on software, AI systems, and practical engineering work.';
